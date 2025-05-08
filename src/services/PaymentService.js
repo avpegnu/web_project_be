50f28323843e378b305d840baead67f81d991f20
avpegnu <vietanh142004@gmail.com>
2025-05-08 18:58:20 +0700

    fix

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -53,7 +53,8 @@ const createPaymentGateway = (req, total, order_id) => {
   let hmac = crypto.createHmac("sha512", secretKey);
   let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
   vnp_Params["vnp_SecureHash"] = signed;
-  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+  // vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+  vnpUrl = returnUrl;
   console.log("vnpUrl: ", vnpUrl);
   console.log("vnp_Params: ", vnp_Params);
   return {
