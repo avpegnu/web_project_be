92eaf5187aaedafb7be7b8f0cd865d7d025bfdd5
avpegnu <vietanh142004@gmail.com>
2025-05-08 20:20:11 +0700

    fix

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -61,19 +61,28 @@ const createPaymentGateway = (req, total, order_id) => {
   };
 };
 
+// function sortObject(obj) {
+//   let sorted = {};
+//   let str = [];
+//   let key;
+//   for (key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       str.push(encodeURIComponent(key));
+//     }
+//   }
+//   str.sort();
+//   for (key = 0; key < str.length; key++) {
+//     sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+//   }
+//   return sorted;
+// }
+
 function sortObject(obj) {
-  let sorted = {};
-  let str = [];
-  let key;
-  for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      str.push(encodeURIComponent(key));
-    }
-  }
-  str.sort();
-  for (key = 0; key < str.length; key++) {
-    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
-  }
+  const sorted = {};
+  const keys = Object.keys(obj).sort();
+  keys.forEach((key) => {
+    sorted[key] = obj[key];
+  });
   return sorted;
 }
 
