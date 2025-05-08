6f48bbf3132fa6b59697beb00198b288d62d2e4c
avpegnu <vietanh142004@gmail.com>
2025-05-08 19:44:36 +0700

    fix

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -48,14 +48,13 @@ const createPaymentGateway = (req, total, order_id) => {
   vnp_Params = sortObject(vnp_Params);
 
   let querystring = require("qs");
-  let signData = querystring.stringify(vnp_Params, { encode: true });
+  let signData = querystring.stringify(vnp_Params, { encode: false });
   let crypto = require("crypto");
   let hmac = crypto.createHmac("sha512", secretKey);
   let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
   vnp_Params["vnp_SecureHash"] = signed;
-  vnpUrl = "?" + querystring.stringify(vnp_Params, { encode: true });
-  console.log("vnpUrl: ", vnpUrl);
-  console.log("vnp_Params: ", vnp_Params);
+  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+
   return {
     message: "Payment gateway created successfully",
     data: vnpUrl,
